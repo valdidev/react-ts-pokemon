7d65a946b6bdd9e0be3ea61062c2e9431d7c8a6f
“valdidev” <iamvaldidev@gmail.com>
2023-01-10 21:22:50 +0100

    wip: context working well

--- src/context/PokemonContext.tsx
+++ src/context/PokemonContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { PokemonType } from "../interfaces/types";
 
 interface ContextProps {
@@ -8,17 +8,25 @@ interface ContextProps {
   changeTypeSelected: (type: PokemonType) => void;
 }
 
-export const PokemonContext = createContext({} as ContextProps);
+export const PokemonContext = createContext<ContextProps>({} as ContextProps);
 
-const PokemonProvider = (
-  {
-    /* children */
-  }
-) => {
+const PokemonProvider = ({ children }: any) => {
+  const endpointAllPokemon =
+    "https://pokeapi.co/api/v2/pokemon?limit=10000&offset-0";
+
+  const [pokemon, setPokemon] = useState(null);
+  const [filterdPokemon, setFilteredPokemon] = useState(null);
+
+  const defaultState: PokemonType = {
+    name: "All",
+    url: endpointAllPokemon,
+  };
+
+  const [types, setTypes] = useState([defaultState]);
+  const [filterSelected, setFilterSelected] = useState(defaultState);
+
   return (
-    <PokemonContext.Provider value={{}}>
-      {/* children */}
-    </PokemonContext.Provider>
+    <PokemonContext.Provider value={{}}>{children}</PokemonContext.Provider>
   );
 };
 
