6402b462cc7585c44a1f5a0ef26b6defe5f023fc
“valdidev” <iamvaldidev@gmail.com>
2023-01-11 16:34:13 +0100

    feat: provider implemented

--- src/context/PokemonContext.tsx
+++ src/context/PokemonContext.tsx
@@ -1,12 +1,12 @@
 import axios from "axios";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { AllPokemonResultType, PokemonType } from "../interfaces/types";
 
 interface ContextProps {
   types: PokemonType[];
   filterSelected: PokemonType;
   filteredPokemon: string[] | null;
-  changeTypeSelected: (type: PokemonType) => void;
+  changeSelectedType: (type: PokemonType) => void;
 }
 
 export const PokemonContext = createContext<ContextProps>({} as ContextProps);
@@ -15,16 +15,17 @@ const PokemonProvider = ({ children }: any) => {
   const endpointAllPokemon =
     "https://pokeapi.co/api/v2/pokemon?limit=10000&offset-0";
 
-  const [pokemon, setPokemon] = useState(null);
-  const [filterdPokemon, setFilteredPokemon] = useState(null);
-
   const defaultState: PokemonType = {
     name: "All",
     url: endpointAllPokemon,
   };
 
+  const [pokemon, setPokemon] = useState(null);
+  const [filteredPokemon, setFilteredPokemon] = useState(null);
   const [types, setTypes] = useState([defaultState]);
   const [filterSelected, setFilterSelected] = useState(defaultState);
+
+  const changeSelectedType = () => {};
 
   const getAllPokemon = async () => {
     const { data } = await axios.get(endpointAllPokemon);
@@ -36,9 +37,22 @@ const PokemonProvider = ({ children }: any) => {
     );
   };
 
-  /* return (
-    <PokemonContext.Provider value={{}}>{children}</PokemonContext.Provider>
-  ); */
+  useEffect(() => {
+    getAllPokemon();
+  }, []);
+
+  return (
+    <PokemonContext.Provider
+      value={{
+        types,
+        filterSelected,
+        changeSelectedType,
+        filteredPokemon,
+      }}
+    >
+      {children}
+    </PokemonContext.Provider>
+  );
 };
 
-export { PokemonProvider };
+export default PokemonProvider;
