b5fb7a079f5721d1802efcbfc9535a1970c018fd
“valdidev” <iamvaldidev@gmail.com>
2023-01-13 17:16:06 +0100

    wip: pokemon detail page

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,3 +1,11 @@
+import { useParams } from "react-router-dom";
+import { usePokemon } from "../../hooks/usePokemon";
+
 export const Details = () => {
+  const { pokemonId } = useParams();
+  const { pokemon } = usePokemon("", pokemonId);
+
+  console.log(pokemon);
+
   return <div>Details</div>;
 };
