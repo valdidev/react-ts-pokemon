52fefda64811a65efe4c1adc75232bf7cf967379
“valdidev” <iamvaldidev@gmail.com>
2023-01-15 17:49:50 +0100

    deploy: preparing to deploy

--- src/common/Loader/Loader.tsx
+++ src/common/Loader/Loader.tsx
@@ -1,15 +1,19 @@
-import styles from "./styles.modules.scss"
+import loader from "./loader.module.scss";
 
 interface Props {
   color: string;
   size?: number;
 }
 
-export const Loader = ({ color, size }: Props) => {
+export const Loader = ({ size, color }: Props) => {
   return (
     <span
-      style={{ width: size, height: size, borderColor: color }}
-      className={styles.loader}
+      style={{
+        width: size,
+        height: size,
+        borderColor: color,
+      }}
+      className={loader.loader}
     />
   );
 };
